ad6f4e17e478171c0d39b598ac4838e402b93d2b
irsyadrisno <irsyadrisno@gmail.com>
2024-04-25 15:19:21 +0700

    2

--- components/navbar.vue
+++ components/navbar.vue
@@ -34,7 +34,7 @@
             </ul>
           </div>
         </div>
-        <button>contact</button>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Contact</button>
       </div>
     </nav>
   </div>
