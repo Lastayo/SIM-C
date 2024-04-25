261623c2439896b9e841741b57adb54173dfbc1c
irsyadrisno <irsyadrisno@gmail.com>
2024-04-25 10:10:40 +0700

    navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -3,21 +3,29 @@
       <nav class="bg-white-800 p-4">
         <div class="container mx-auto flex justify-between items-center">
           <a class=" text-lg font-semibold" href="#">Logo</a>
-          <ul class="flex space-x-4">
+          <ul class="flex  space-x-10 font-semibold">
             <li>
               <a href="#" class=" hover:text-white">Home</a>
             </li>
             <li>
-              <a href="#" class=" hover:text-white">About Us</a>
+              <a href="#" class="  hover:text-white">About Us</a>
             </li>
             <li>
-              <a href="#" class=" hover:text-white">Goals</a>
+              <a href="#" class="  hover:text-white">Goals</a>
             </li>
             <li>
-              <a href="#" class=" hover:text-white">service</a>
+              <a href="#" class="  hover:text-white">service</a>
             </li>
             <li>
-              <a href="#" class=" hover:text-white">Our Teaam</a>
+              <a href="#" class="  hover:text-white">Our Team</a>
+            </li>
+          </ul>
+          <ul class="flex space-x-3 font-semibold ">
+            <li>
+              <a href="#/" class=" hover:underline focus:underline focus-visible:underline">ID</a>
+            </li>
+            <li>
+              <a href="#" class=" hover:underline  focus:underline focus-visible:underline">EN</a>
             </li>
           </ul>
         </div>
