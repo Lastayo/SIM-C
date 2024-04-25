171cd909877ea1d234b2ba6f2af8ef3abc249f01
irsyadrisno <irsyadrisno@gmail.com>
2024-04-25 15:14:30 +0700

    navabr mari

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,39 +1,41 @@
 <template>
-    <div>
-      <nav class="bg-white-800 p-4">
-        <div class="container mx-auto flex justify-between items-center">
+  <div>
+    <nav class="bg-white-800 p-4">
+      <div class="container mx-auto flex justify-between items-center">
+        <div class="flex space-x-10  items-center ">
           <a class=" text-lg font-semibold" href="#">Logo</a>
-          <div class="flex  space-x-10 ">
-          <ul class="flex space-x-10 font-semibold">
-            <li>
-              <a href="#" class=" hover:text-white">Home</a>
-            </li>
-            <li>
-              <a href="#" class="  hover:text-white">About Us</a>
-            </li>
-            <li>
-              <a href="#" class="  hover:text-white">Goals</a>
-            </li>
-            <li>
-              <a href="#" class="  hover:text-white">service</a>
-            </li>
-            <li>
-              <a href="#" class="  hover:text-white">Our Team</a>
-            </li>
-          </ul>
-          <ul class="flex space-x-3 font-semibold ">
-            <li>
-              <a href="#/" class=" text-black  hover:border-b hover:border-blue-800  focus:border-b focus:border-blue-800  focus-visible:border-b focus-visible:border-blue-800">ID</a>
-            </li>
-            <li>
-              <a href="#" class=" text-black  hover:border-b hover:border-blue-800  focus:border-b focus:border-blue-800  focus-visible:border-b focus-visible:border-blue-800">EN</a>
-            </li>
-          </ul>
+          <div class="flex space-x-10 ">
+            <ul class="flex space-x-10 font-semibold">
+              <li>
+                <a href="#" class=" hover:text-white">Home</a>
+              </li>
+              <li>
+                <a href="#" class="  hover:text-white">About Us</a>
+              </li>
+              <li>
+                <a href="#" class="  hover:text-white">Goals</a>
+              </li>
+              <li>
+                <a href="#" class="  hover:text-white">Service</a>
+              </li>
+              <li>
+                <a href="#" class="  hover:text-white">Our Team</a>
+              </li>
+            </ul>
+            <ul class="flex space-x-3 font-semibold ">
+              <li>
+                <a href="#/"
+                  class=" text-black  hover:border-b hover:border-blue-800  focus:border-b focus:border-blue-800  focus-visible:border-b focus-visible:border-blue-800">ID</a>
+              </li>
+              <li>
+                <a href="#"
+                  class=" text-black  hover:border-b hover:border-blue-800  focus:border-b focus:border-blue-800  focus-visible:border-b focus-visible:border-blue-800">EN</a>
+              </li>
+            </ul>
+          </div>
         </div>
-          <button>contact</button>
-        </div>
-      </nav>
-    </div>
-  </template>
-  
-  
+        <button>contact</button>
+      </div>
+    </nav>
+  </div>
+</template>
